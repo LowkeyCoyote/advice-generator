c5246d605b585500ffffd0b429f67a66f6843e2d
JoskaGyuricza <joska.gyur@gmail.com>
2022-03-23 13:33:00 +0100

    first version project

--- src/container/AdviceGenerator/AdviceGenerator.js
+++ src/container/AdviceGenerator/AdviceGenerator.js
@@ -1,6 +1,7 @@
 import "./AdviceGenerator.css";
 import diviserImgDesktop from "../../images/pattern-divider-desktop.svg";
 import diviserImgMobile from "../../images/pattern-divider-mobile.svg";
+import dice from "../../images/icon-dice.svg"
 import axios from "axios";
 import {useEffect, useState} from "react";
 
@@ -34,7 +35,7 @@ function AdviceGenerator(){
                 <img className="img-mobile" src={diviserImgMobile} alt="mobile"/>
             </div>
             <div className="dice" onClick={adviceAPI}>
-                <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z" fill="#202733"/></svg>
+                <img src={dice} alt="dice"/>
             </div>
         </section>
     )
